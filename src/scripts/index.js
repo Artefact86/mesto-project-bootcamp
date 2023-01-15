9baf3de734a83862bb49a31ad0f9a09088dc6411
ArtemK <artekan@yandex.ru>
2023-01-15 21:27:05 +0300

    сделал кнопку неактивной

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,5 @@
 import '../pages/index.css'; // добавьте импорт главного файла 
-import { showInputError } from './valid.js';
+//import { showInputError } from './valid.js';
 import { initialCards } from './data.js';
 
 const buttonPopupProfile = document.querySelector('.profile__edit-button');
@@ -26,7 +26,76 @@ const popupImgType = popupImg.querySelector('.popup__image');
 const popupImgCaption = popupImg.querySelector('.popup__caption');
 const elements = document.querySelector('.elements');
 
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+const formError = formElement.querySelector(`#${formInput.id}-error`);
 
+const errorMessage = {
+  empty: 'Вы пропустили это поле',
+  wrongLenght: 'Введите от 2 до 30 символов',
+  wrongUrl: 'Тут должна быть ссылка',
+};
+
+
+export const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.add('popup__input_error');
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add('form__input-error_active');
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
+  inputElement.classList.remove('popup__input_error');
+  errorElement.classList.remove('form__input-error_active');
+  errorElement.textContent = '';
+};
+
+const isValid = (formElement, inputElement) => {
+  if(!inputElement.validity.valid){
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+  const buttonElement = formElement.querySelector('.popup__save-button');
+  //toggleButtonState(inputList, buttonElement);
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input',function() {
+      isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
+    });
+    
+  });
+  
+};
+
+const enableValidation = () => {
+  const formList = Array.from(document.querySelectorAll('.popup__form'));
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
+//функция для проверки на валидность всех полей 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.classList.add('popup__save-button_inactive');
+  } else {
+    buttonElement.classList.remove('popup__save-button_inactive');
+  }
+};
 
 // функция открытия попапа
 function openPopup(popup){
